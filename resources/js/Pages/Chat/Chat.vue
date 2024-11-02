2b8959807526285e16fcccd5126c06886051ebc9
Y2theK <yeyintkyaw.dev@gmail.com>
2024-11-02 21:48:35 +0630

    fix: bug fixed

--- resources/js/Pages/Chat/Chat.vue
+++ resources/js/Pages/Chat/Chat.vue
@@ -87,46 +87,74 @@ onMounted(() => {
         });
 
     window.Echo.join(`conversation.${props.conversation.id}`)
-            .here(users => {
-                onlineUsers.value = users;
-                // console.log(users);
-            })
-            .joining(user => {
-                onlineUsers.value.push(user);
-            })
-            .leaving(user => {
-                onlineUsers.value =onlineUsers.value.filter((u) => u.id !== user.id);
-            })
+        .here((users) => {
+            onlineUsers.value = users;
+            // console.log(users);
+        })
+        .joining((user) => {
+            onlineUsers.value.push(user);
+        })
+        .leaving((user) => {
+            onlineUsers.value = onlineUsers.value.filter(
+                (u) => u.id !== user.id
+            );
+        });
 });
 
-    onBeforeUnmount(() => {
-        
-        window.Echo.leave(`conversation.${props.conversation.id}`,user => {
-            
-            onlineUsers.value = onlineUsers.value.filter((u) => u.id !== user.id);
-        })
-    })
+onBeforeUnmount(() => {
+    window.Echo.leave(`conversation.${props.conversation.id}`, (user) => {
+        onlineUsers.value = onlineUsers.value.filter((u) => u.id !== user.id);
+    });
+});
 </script>
 
 <template>
-    <Dashboard :conversations="conversations">
-        <div class="flex items-center bg-gray-100  border-b py-4 rounded-xl px-4" >
-            <div v-if="users.length === 1" class="text-lg font-semibold mr-2 p-2 rounded">
-                {{ users[0].name }}
-                <span :class="onlineUsers.find((u) => u.id == user.id) ? 'bg-green-500' : 'bg-red-400'" class="inline-block h-2 w-2 rounded-full"></span>
-            </div>
 
-            <div  class="flex items-center bg-gray-100  border-b py-4 rounded-xl px-4" v-else>
-                <span class="text-lg font-semibold mx-2 bg-indigo-100 p-2 rounded" v-show="conversation.name">{{ conversation.name.toUpperCase() }}</span>
-                <div v-for="user in onlineUsers" :key="user.id" class="" >
-                    <div class="text-lg font-semibold mr-2 border p-2 rounded" v-if="user.id !== auth.user.id">
-                        {{ user.name }} 
-                        <span :class="onlineUsers.find((u) => u.id == user.id) ? 'bg-green-500' : 'bg-red-400'" class="inline-block h-2 w-2 rounded-full"></span>
-                    </div>
+    <Dashboard :conversations="conversations">
+
+        <div
+            class="flex items-center bg-gray-100 border-b py-4 rounded-xl px-4"
+            v-if="users.length === 1"
+        >
+            <div class="text-lg font-semibold mr-2 p-2 rounded">
+                {{ users[0].name }}
+                <span
+                    :class="
+                        onlineUsers.find((u) => u.id == users[0].id)
+                            ? 'bg-green-500'
+                            : 'bg-red-400'
+                    "
+                    class="inline-block h-2 w-2 rounded-full"
+                ></span>
+            </div>
+        </div>
+        <div
+            class="flex items-center bg-gray-100 border-b py-4 rounded-xl px-4"
+            v-else
+        >
+            <span
+                class="text-lg font-semibold mx-2 bg-indigo-100 p-2 rounded"
+                v-show="conversation.name"
+                >{{ conversation.name.toUpperCase() }}</span
+            >
+            <div v-for="user in onlineUsers" :key="user.id" class="">
+                <div
+                    class="text-lg font-semibold mr-2 border p-2 rounded"
+                    v-if="user.id !== auth.user.id"
+                >
+                    {{ user.name }}
+                    <span
+                        :class="
+                            onlineUsers.find((u) => u.id == user.id)
+                                ? 'bg-green-500'
+                                : 'bg-red-400'
+                        "
+                        class="inline-block h-2 w-2 rounded-full"
+                    ></span>
                 </div>
             </div>
-            
         </div>
+
         <div
             class="flex flex-col h-full overflow-x-auto mb-4 [&::-webkit-scrollbar]:w-1 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-gray-300 dark:[&::-webkit-scrollbar-thumb]:bg-gray-400"
             ref="messageContainer"
@@ -215,7 +243,6 @@ onMounted(() => {
                             v-model="form.message"
                             required
                             @keydown="sendTypingEvent"
-                            
                         />
                         <button
                             class="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600"
